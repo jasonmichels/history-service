c58d31210b517557cb7ea9e3a071bf96f013736e
Jason Michels <thebizztech@me.com>
2015-04-23 21:21:11 -0500

    - Added routing by client and key to retrieve data from soon to be setup database

--- routes/history.js
+++ routes/history.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET history listing page. */
-router.get('/', function(req, res, next) {
+router.get('/:client/:key', function(req, res, next) {
+  var client = req.params.client;
+  var key = req.params.key;
+
+  // now that we have client and key, we need to get all the data for that client/key combination
+
   res.render('history', { title: 'History Title Page' });
 });
 
