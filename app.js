5e54e3b5f30020d24e7f94d7935cbf2d368b8cb5
Jason Michels <thebizztech@me.com>
2015-04-24 20:54:08 -0500

    - Removed views so just return json. Have mostly working implementation of finding all records by key and inserting a database record.  Added todo notes

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,19 +7,10 @@ var routes = require('./routes/history');
 
 var app = express();
 
-// view engine setup
-var handlebars  = require('express-handlebars').create({ defaultLayout:'main' });
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/history', routes);
 
@@ -33,16 +22,14 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
+// @TODO implement better error handling
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.json({ message: err.message });
   });
 }
 
@@ -50,10 +37,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.json({ message: err.message });
 });
 
 
