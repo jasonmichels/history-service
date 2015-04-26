173816a87bceb4a1df1b2dedaffd139ef43cde86
Jason Michels <thebizztech@me.com>
2015-04-25 22:29:53 -0500

    - Cleaned up the put request and actually made it a put request and are getting the json from the body of the request

--- routes/history.js
+++ routes/history.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var historyRepository = require('../repositories/historyRepository');
 
-/* GET history listing page. */
+/**
+ * Get all history for a key
+ */
 router.get('/:client/:key', function(req, res, next) {
 
   var collectionKey = req.params.client + '/' + req.params.key;
@@ -13,23 +15,16 @@ router.get('/:client/:key', function(req, res, next) {
 
 });
 
-/* PUT save an item to the database. */
-router.get('/put/:client/:key', function(req, res, next) {
+/**
+ * Save a history item to the database if it does not already exist
+ */
+router.put('/:client/:key', function(req, res, next) {
 
   var collectionKey = req.params.client + '/' + req.params.key;
+  var json = req.body;
 
   // Return quick and continue processing database request
   res.json({ message: "Inserted item into database", status: "success" });
-
-  // @TODO Get this from the body of the request
-  var json = {
-    "address" : {
-      "street" : "123 main st",
-      "zipcode" : "55410"
-    },
-    "name" : "Jason",
-    "restaurant_id" : "9999999"
-  };
 
   historyRepository.insertOrUpdateIfExists(collectionKey, json, function () {
     console.log("Inserted a document into the collection. " + collectionKey);
