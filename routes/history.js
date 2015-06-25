9c5a772e2df77e48cb3e6e92ea6e885b1f62eb59
Jason Michels <michelsja@icloud.com>
2015-06-25 14:47:28 -0500

    Added ability to authenticate users with an access token against a service that returns user credentials

--- routes/history.js
+++ routes/history.js
@@ -2,21 +2,37 @@ var express = require('express');
 var router = express.Router();
 var fetchAllHistoryCommandHandler = require('../commands/fetchAllHistoryCommandHandler');
 var saveHistoryCommandHandler = require('../commands/saveHistoryCommandHandler');
+var AccessTokenAuthentication = require('../lib/accessTokenAuthentication');
 
 /**
  * Get all history for a key
  */
 router.get('/:client/:key', function(req, res, next) {
 
-    var collectionKey = req.params.client + '/' + req.params.key;
+    var Authentication = new AccessTokenAuthentication();
 
-    fetchAllHistoryCommandHandler.handle(collectionKey, function (err, result) {
-        if (err) {
-            res.json({error: err, message: "An error occurred trying to access history"});
-        } else {
-            res.json(result);
-        }
+    Authentication.on('success', function(user) {
+
+        var collectionKey = req.params.client + '/' + req.params.key;
+
+        fetchAllHistoryCommandHandler.handle(collectionKey, function (err, result) {
+            if (err) {
+                res.status(400);
+                res.json({error: err, message: "An error occurred trying to access history"});
+            } else {
+                res.json(result);
+            }
+        });
+
+    }).on('responseError', function (result) {
+        res.status(401);
+        res.json(result);
+    }).on('error', function(err) {
+        res.status(500);
+        res.json({error: err, message: "Unable to connect to user authentication service"});
     });
+
+    Authentication.handle(req.header('access_token'));
 
 });
 
@@ -25,15 +41,28 @@ router.get('/:client/:key', function(req, res, next) {
  */
 router.put('/:client/:key', function(req, res, next) {
 
-    var collectionKey = req.params.client + '/' + req.params.key;
-    var json = req.body;
+    var Authentication = new AccessTokenAuthentication();
 
-    // Return quick and continue processing database request
-    res.json({ message: "Processing history item", status: "success" });
+    Authentication.on('success', function(user) {
+        var collectionKey = req.params.client + '/' + req.params.key;
+        var json = req.body;
 
-    saveHistoryCommandHandler.handle(collectionKey, json, function (err, result) {
-        // Circuit breaker already took care of everything, and user is gone, so not much to do here
+        // Return quick and continue processing database request
+        res.json({ message: "Processing history item", status: "success" });
+
+        saveHistoryCommandHandler.handle(collectionKey, json, function (err, result) {
+            // Circuit breaker already took care of everything, and user is gone, so not much to do here
+        });
+
+    }).on('responseError', function (result) {
+        res.status(401);
+        res.json(result);
+    }).on('error', function(err) {
+        res.status(500);
+        res.json({error: err, message: "Unable to connect to user authentication service"});
     });
+
+    Authentication.handle(req.header('access_token'));
 
 });
 
